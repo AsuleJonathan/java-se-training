40ba785f1519ca5f8a162caa0cdc60c181c54799
AsuleJonathan <asulejona@gmail.com>
2023-09-07 02:49:34 -0700

    fix: stings.java file

--- src/main/java/com/systech/stringsandstringBuilders/Strings.java
+++ src/main/java/com/systech/stringsandstringBuilders/Strings.java
@@ -1,3 +1,26 @@
-public static void main(String[] args) {
-    
+package com.systechtraining.stringsandstringBuilders;
+
+import java.util.logging.Logger;
+
+public class Strings {
+    private static final Logger LOGGER = Logger.getLogger(Strings.class.getName());
+
+    public static void main(String[] args) {
+
+        String message = "Hello";
+      
+        message.concat(" world!"); 
+      
+        message =  message.concat(" Programmer!"); 
+        LOGGER.info(message);
+        
+        String sb = new String("Hello, world");
+        String sa = "Hello, world";
+        boolean equals = sa.equals(sb);
+        boolean equals2 = sa == sb;
+
+        LOGGER.info(""+equals);
+        LOGGER.info(""+equals2);
+    }
+
 }
