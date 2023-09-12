e5391d502c8862d663231fdb73cc145270025d6c
AsuleJonathan <asulejona@gmail.com>
2023-09-12 17:17:32 +0300

    Feat: Add contants utility

---
+++ src/main/java/com/systech/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.systech.constants;
+
+public class Constants {
+    public static final double RATE = 0.02;  //Constants.rate - how to use it in other files. - 
+    public static final String DEFAULT_PASSWORD = "Admin123";
+    
+    
+}
+
