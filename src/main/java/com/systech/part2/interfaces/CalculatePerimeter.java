a6800d1f470bfdd73eca5494dcd594ef39000efe
AsuleJonathan <asulejona@gmail.com>
2023-09-23 12:51:48 +0300

    Update: pom source level

--- src/main/java/com/systech/part2/interfaces/CalculatePerimeter.java
+++ src/main/java/com/systech/part2/interfaces/CalculatePerimeter.java
@@ -2,7 +2,9 @@ package com.systech.part2.interfaces;
 
 public interface CalculatePerimeter {
     double calculatePerimeter(double x, double y);
-    default double sum (int a, int b) {
+
+    // ! Java 1.8+ => You can have default implementation logic on the interface
+    default double sum(int a, int b) {
         return 0;
     }
 
