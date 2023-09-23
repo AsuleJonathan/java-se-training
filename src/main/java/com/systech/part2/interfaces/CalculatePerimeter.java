79c8aa760332d2e2c1b3d661619b4d81e0873e07
AsuleJonathan <60647098+AsuleJonathan@users.noreply.github.com>
2023-09-23 12:42:20 +0300

    Delete src/main/java/com/systech/part2/interfaces/CalculatePerimeter.java

--- src/main/java/com/systech/part2/interfaces/CalculatePerimeter.java
+++
@@ -1,9 +0,0 @@
-package com.systech.part2.interfaces;
-
-public interface CalculatePerimeter {
-    double calculatePerimeter(double x, double y);
-    default double sum (int a, int b) {
-        return 0;
-    }
-
-}
