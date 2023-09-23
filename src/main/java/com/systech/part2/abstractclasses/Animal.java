26414815d73c04e9d50c37c9b3f78bc4a7b2e3e0
AsuleJonathan <asulejona@gmail.com>
2023-09-23 12:08:53 +0300

    working with abstract classes

---
+++ src/main/java/com/systech/part2/abstractclasses/Animal.java
@@ -0,0 +1,17 @@
+package com.systech.part2.abstractclasses;
+
+public abstract class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+    
+    public abstract void makeSound();
+
+    public void  eat(){
+        System.out.println(name + " is eating");
+    }
+}
+
+
